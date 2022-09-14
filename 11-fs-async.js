3c65d533093de02a2dbd2eeefd7cbe60c15f04b9
zohde <devoweb8@mail.com>
2022-09-14 17:49:20 +0300

    first commit

---
+++ 11-fs-async.js
@@ -0,0 +1,31 @@
+const { readFile, writeFile } = require("fs");
+console.log('start')
+// if you dont put utf8 in between you will get back buffer err
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    // console.log(first, second)
+    writeFile(
+      "./content/result-async.txt",
+      `Here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('done with this task');
+      }
+    );
+  });
+});
+
+console.log('starting with next task')
---
+++ 4-names.js
@@ -0,0 +1,11 @@
+const secret = "SUPER SECRET";
+const john = "john";
+const peter = "peter";
+
+
+// console.log(module)
+
+module.exports = {
+    john,
+    peter
+}
